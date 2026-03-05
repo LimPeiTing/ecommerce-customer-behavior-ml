8a9b9b8894a889230091595ca3d5b7c05a8dc698
Lim Pei Ting <peiting1202@gmail.com>
2026-03-05 16:34:03 +0800

    Create model_evaluation.R

---
+++ src/model_evaluation.R
@@ -0,0 +1,9 @@
+evaluate_models <- function(reg_model, class_model){
+
+  print("Regression Model Summary")
+  print(summary(reg_model))
+
+  print("Classification Model")
+  print(class_model)
+
+}
